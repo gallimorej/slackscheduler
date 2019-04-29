e7a1443b0cb21de7f94b37c6721448999dd8458a
gallimorej <jeff.gallimore@excella.com>
2019-04-28 20:06:28 -0400

    Finish basic config and setup

---
+++ Makefile
@@ -0,0 +1,9 @@
+#
+# entry points
+#
+
+autotest:
+	lein test-refresh :growl
+
+slackscheduler:
+	lein run
--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
 (defproject slackscheduler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A command line utility to schedule messages in a Slack workspace"
+  :url "https://github.com/gallimorej/slackscheduler"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot slackscheduler.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "0.2.6"]]
+
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  ;:hooks [environ.leiningen.hooks]
+  :profiles {:production {:env {:production true}}
+             :dev        {:plugins [[com.jakemccrary/lein-test-refresh "LATEST"]]}
+             :uberjar    {:aot :all}}
+
+  :main slackscheduler.core)
